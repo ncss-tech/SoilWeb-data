4eb4b886ac43713e81c0813ade85329f3b88af60
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2025-12-01 10:32:31 -0800

    update, note some AK series with NA taxsuborder

--- code/prepare-SC-from-NASIS.R
+++ code/prepare-SC-from-NASIS.R
@@ -1,13 +1,14 @@
-## 2023-08-17
+## 2025-12-01
 ## D.E. Beaudette
-## Export the current SC database from local NASIS DB
+## Export the current SC database from local NASIS DB for use in SoilWeb and beyond
 ##
 
 library(soilDB)
 
-# refresh local NASIS
+# refresh local NASIS first
 sc <- get_soilseries_from_NASIS()
 
+# 2025-12-01: 26573
 nrow(sc)
 
 # remove edit history
@@ -15,3 +16,13 @@ sc$soilseriesedithistory <- NULL
 
 write.csv(sc, file = gzfile('files/SC-database.csv.gz'), row.names = FALSE)
 
+
+## bugs and questions
+
+# # 2025-11-30: there are some AK soil series missing suborder labels
+# z <- sc[which(sc$soilseriesstatus == 'established' & is.na(sc$taxsuborder)), ]
+# 
+# knitr::kable(
+#   z[, c('mlraoffice', 'soilseriesname', 'taxorder', 'taxsuborder', 'taxgrtgroup', 'taxsubgrp')],
+#   row.names = FALSE
+# )
